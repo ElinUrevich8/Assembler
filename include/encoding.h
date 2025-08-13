7ad9704a387e2094c93f63556e14be64b3aec307
Elin Urevich <elinureivch@gmail.com>
2025-08-13 03:47:27 +0300

    case3 now produce small error

--- include/encoding.h
+++ include/encoding.h
@@ -11,7 +11,7 @@
  */
 void strip_comment_inplace(char *s);
 
-/* ===== Addressing modes (bitmask for legality tables) =====
+/*  Addressing modes (bitmask for legality tables) 
  * Note: the 2-bit FIELD CODES in the machine word are defined in isa.h.
  * These values are only for "allowed modes" masks and APIs.
  */
@@ -23,16 +23,16 @@ typedef enum {
     ADR_REGISTER  = 1 << 3   /* code 3 in first word */
 } AddrMode;
 
-/* ===== Pass 1 sizing info ===== */
+/*  Pass 1 sizing info  */
 typedef struct { int words; int operands; } EncodedInstrSize;
 
-/* ===== Directives parsers (data and strings) ===== */
+/*  Directives parsers (data and strings)  */
 bool parse_symbol_name(const char *s, char *out, size_t outsz);
 int  parse_and_push_data_operands(const char *s, CodeImg *data, Errors *errs, int lineno);
 int  parse_and_push_string(const char *s, CodeImg *data, Errors *errs, int lineno);
 int  parse_and_push_mat(const char *s, CodeImg *data, Errors *errs, int lineno);
 
-/* ===== Instruction sizing / opcode lookup ===== */
+/*  Instruction sizing / opcode lookup  */
 bool encoding_estimate_size(const char *instr, EncodedInstrSize *sz, Errors *errs, int lineno);
 bool encoding_lookup_opcode(const char *mnemonic, int *out_opcode, int *out_argc);
 
---
+++ tests/cases/pass2/case5/test.as
@@ -0,0 +1,4 @@
+muv #1,r2
+EXT_LAB: .data 14,,4
+;comment
+gblhlm5569323232323mvcxxxvvxzvxvxfvclkkmj87hyid74: .string "too long"
